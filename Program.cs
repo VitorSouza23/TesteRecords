5855ec13c4e306e7cd962a975f6df6eb208d2a12
Vitor de Souza <vitordesolza@gmail.com>
2021-02-08 12:01:00 -0300

    Testando comparações

--- Program.cs
+++ Program.cs
@@ -5,3 +5,14 @@ var bonacera1 = new Bonacera { Name = "Natasha" };
 WriteLine(bonacera1.SayBonacera());
 var bonacera2 = bonacera1 with { Name = "Katucha" };
 WriteLine(bonacera2.SayBonacera());
+
+var bonacera3 = new Bonacera {Name = "Natasha"};
+WriteLine(bonacera3 == bonacera1);
+WriteLine(bonacera3 == bonacera2);
+
+WriteLine(bonacera1);
+WriteLine(bonacera2);
+WriteLine(bonacera3);
+
+var clone  = bonacera3 with {};
+WriteLine(clone);
